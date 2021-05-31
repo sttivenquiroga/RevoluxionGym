6ba160c183e5cad37d07ba7b7d33bce6331b659c
sttivenquiroga <sttivenquiroga@gmail.com>
2021-05-31 10:48:53 -0500

    Includes models and routes of user and nutrition plans

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,28 @@
-// Importamos los modulos expess y mongoose
 const express = require("express");
 const mongoose = require("mongoose");
+const Auth = require("./routes/auth");
+const User = require("./routes/user");
+const NutritionPlan = require("./routes/nutritionPlans");
 
-// Importamos rutas
-//const Auth = require("./routes/auth");
-
-// Variable app la cual ejecutara nuestra aplicación
 const app = express();
 
-// En la aplicación api usaremos jsons para la comunicación
 app.use(express.json());
-//app.use("/api/auth/", Auth);
+app.use("/api/auth/", Auth);
+app.use("/api/user/", User);
+app.use("/api/nutritionPlan/", NutritionPlan);
 
-// Tipos de puerto que tiene nuestra aplicacion ( Left: Hosting and Right: Local )
 const port = process.env.PORT || 3001;
 
-// Evento el cual nos informa el estado del servidor 
-app.listen(port, () => console.log("Servidor API habilitado en el port: " + port));
+app.listen(port, () =>
+  console.log("Servidor API habilitado en el port: " + port)
+);
 
-// Conexion directa a MongoDB (NoSQL) usando la promesa connect que pertenece a la libreria mongoose
-mongoose.connect("mongodb://localhost:27017/revoluxiongymdb", {
+mongoose
+  .connect("mongodb://localhost:27017/revoluxiongymdb", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false, 
-    useCreateIndex: true
-})
-.then(() => console.log("Conexion con MongoDB successful"))
-.catch((err) => console.log("Conexion con MongoDB no exitosa error: " + err));
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Conexion con MongoDB successful"))
+  .catch((err) => console.log("Conexion con MongoDB no exitosa error: " + err));
