461ff76df1d47fd3e284de790fc75a2d74c42924
Miguel199627 <cerquera199627@hotmail.com>
2021-06-01 12:04:48 -0500

    Reforzamiento de la seguridad en middleware user, routes plan, rol and status payment

--- backend/routes/rol.js
+++ backend/routes/rol.js
@@ -7,53 +7,56 @@
  const express = require("express");
  const router = express.Router();
  const Rol = require("../models/rol");
- const User = require("../models/user");
  const Auth = require("../middleware/auth");
+ const UserAuth = require("../middleware/user");
  
- let user = {};
- 
- // Created rols
- router.post("/add", Auth, async (req, res) => {
-    !await valiUser(req) ? res.status(400).send("Usuario no autenticado") : user = valiUser(req);
-    if(Object.keys(req.body).length === 0) return res.status(401).send("No vienen campos");
-    if(!req.body.rol) return res.status(401).send("Faltan campos");
+ // Create rol
+ router.post("/add", Auth, UserAuth, async (req, res) => {
+    if(!req.body.rol) return res.status(401).send("Incomplete data");
+
     const rol = new Rol({
       rol: req.body.rol
     });
+
     const result = await rol.save();
-    if (!result) return res.status(401).send("No se pudo agregar el rol");
+    if (!result) return res.status(401).send("Error creating rol");
     return res.status(200).send({ result });
  });
  
- // Read rols
- router.get("/list", Auth, async (req, res) => {
-    !await valiUser(req) ? res.status(400).send("Usuario no autenticado") : user = valiUser(req);
+ // Get all rols
+ router.get("/getAll", Auth, UserAuth, async (req, res) => {
     const rol = await Rol.find();
+    if (!rol) return res.status(401).send("Error fetching rol");
     return res.status(200).send({ rol });
  });
  
- // Updated rols
- router.put("/update", Auth, async (req, res) => {
-    !await valiUser(req) ? res.status(400).send("Usuario no autenticado") : user = valiUser(req);
-    if(Object.keys(req.body).length === 0) return res.status(401).send("No vienen campos");
-    if(!req.body.rol || !req.body._id) return res.status(401).send("Faltan campos");
+ // Update rol
+ router.put("/update", Auth, UserAuth, async (req, res) => {
+    if(!req.body._id || !req.body.rol) return res.status(401).send("Incomplete data");
+
     const rol = await Rol.findByIdAndUpdate(req.body._id, {
-      rol: req.body.rol
+      rol: req.body.rol,
+      status: true
     });
-    if (!rol) return res.status(401).send("No se pudo editar el rol");
+
+    if (!rol) return res.status(401).send("Error updating plan");
     return res.status(200).send({ rol });
  });
- 
- // Delete rols
- router.delete("/:_id", Auth, async (req, res) => {
-    !await valiUser(req) ? res.status(400).send("Usuario no autenticado") : user = valiUser(req);
-    const rol = await Rol.findByIdAndDelete(req.params._id);
-    if (!rol) return res.status(401).send("No se pudo eliminar el rol");
-    return res.status(200).send("Rol eliminado");
- });
- 
- const valiUser = async (req) => {
-    return await User.findById(req.user._id);
- };
+
+// Delete rol
+router.put("/delete", Auth, UserAuth, async (req, res) => {
+   if(
+       !req.body._id ||
+       !req.body.rol
+   ) return res.status(401).send("Incomplete data");
+
+   const rol = await Rol.findByIdAndUpdate(req.body._id, {
+      rol: req.body.rol,
+      status: false
+   });
+
+   if (!rol) return res.status(401).send("Error deleting rol");
+   return res.status(200).send({ rol });
+});
  
  module.exports = router;
