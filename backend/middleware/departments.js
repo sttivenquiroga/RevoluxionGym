c5520aeb76cd8129be6ded95912eeddccff997e1
NataliaOrtizMa <nataliaortizma@gmail.com>
2021-06-01 11:15:43 -0500

    Refactor Department/Cities/Locations routes

--- backend/middleware/departments.js
+++
@@ -1,9 +0,0 @@
-function validateDepartment(req, res, next) {
-    const {department} = req.body;
-    if(!department) { 
-        return res.status(400).send("No se envió el nombre del departamento");
-    }
-    next();
-}
-
-module.exports = validateDepartment;
