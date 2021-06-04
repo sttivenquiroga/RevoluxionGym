2a97d8024045a351c90e9792b7e2b83ec6aef1a2
NataliaOrtizMa <nataliaortizma@gmail.com>
2021-06-04 13:20:13 -0500

    Refactor Depts/Cities/locations files

--- backend/models/departments.js
+++ backend/models/departments.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const departmentsSchema = new mongoose.Schema({
     id: String,
     department: String,
-    status: {type:Boolean, default: true}
+    status: {type:Boolean, default: true},
+    date: {type: Date, default: Date.now}
+
 })
 const Departments = mongoose.model("departments", departmentsSchema);
 
